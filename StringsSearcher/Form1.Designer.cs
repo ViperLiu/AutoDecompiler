a0b4163919147b883c5e878f336a78220d73d7ff
ViperLiu <a0960330@gmail.com>
2018-05-11 08:59:27 +0800

    initial commit

---
+++ StringsSearcher/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace StringsSearcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbOutputDir = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbMD5 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbSHA1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button4 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(12, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(284, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "apk或ipa檔：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(309, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "選擇檔案";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.SelectedPath = "C:\\Users\\劉建毅\\Desktop";
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label2.Location = new System.Drawing.Point(398, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "INFO：";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(309, 55);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 39);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "選擇資料夾";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label3.Location = new System.Drawing.Point(9, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "原始碼輸出資料夾：";
+            // 
+            // tbOutputDir
+            // 
+            this.tbOutputDir.AllowDrop = true;
+            this.tbOutputDir.Location = new System.Drawing.Point(11, 72);
+            this.tbOutputDir.Name = "tbOutputDir";
+            this.tbOutputDir.Size = new System.Drawing.Size(284, 22);
+            this.tbOutputDir.TabIndex = 5;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label5.Location = new System.Drawing.Point(6, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "MD5：";
+            // 
+            // tbMD5
+            // 
+            this.tbMD5.AllowDrop = true;
+            this.tbMD5.Location = new System.Drawing.Point(8, 37);
+            this.tbMD5.Name = "tbMD5";
+            this.tbMD5.ReadOnly = true;
+            this.tbMD5.Size = new System.Drawing.Size(367, 22);
+            this.tbMD5.TabIndex = 10;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbSHA1);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.tbMD5);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(9, 100);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(386, 116);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hash Values";
+            // 
+            // tbSHA1
+            // 
+            this.tbSHA1.AllowDrop = true;
+            this.tbSHA1.Location = new System.Drawing.Point(8, 81);
+            this.tbSHA1.Name = "tbSHA1";
+            this.tbSHA1.ReadOnly = true;
+            this.tbSHA1.Size = new System.Drawing.Size(368, 22);
+            this.tbSHA1.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label6.Location = new System.Drawing.Point(6, 62);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "SHA1：";
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.AllowDrop = true;
+            this.tbOutput.Font = new System.Drawing.Font("新細明體", 11F);
+            this.tbOutput.Location = new System.Drawing.Point(401, 29);
+            this.tbOutput.Multiline = true;
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.ReadOnly = true;
+            this.tbOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbOutput.Size = new System.Drawing.Size(512, 367);
+            this.tbOutput.TabIndex = 13;
+            this.tbOutput.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtFolderPath_DragDrop);
+            this.tbOutput.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtFolderPath_DragEnter);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label4.Location = new System.Drawing.Point(6, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(168, 16);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "ipa檔只能使用hash功能";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(9, 238);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(76, 39);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "輸出原始碼";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(463, 10);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(215, 13);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 16;
+            this.progressBar1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 408);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbOutputDir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "AutoDecompiler";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtFolderPath_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtFolderPath_DragEnter);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbOutputDir;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbMD5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbSHA1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.Label label4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ StringsSearcher/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+using System.Diagnostics;
+using System.Management;
+
+
+namespace StringsSearcher
+{
+    public partial class Form1 : Form
+    {
+        Process process;
+        ProcessStartInfo startInfo = new ProcessStartInfo();
+        public Form1()
+        {
+            InitializeComponent();
+            this.textBox1.DragEnter += new DragEventHandler(txtFolderPath_DragEnter);
+            this.textBox1.DragDrop += new DragEventHandler(txtFolderPath_DragDrop);
+        }
+
+
+        public string getSHA1()
+        {
+            if (textBox1.Text == "")
+                return null;
+            string location = textBox1.Text;
+            byte[] apkByte = File.ReadAllBytes(location);
+            SHA1 sha1 = new SHA1CryptoServiceProvider();//建立一個SHA1
+            byte[] crypto = sha1.ComputeHash(apkByte);//進行SHA1加密
+            string result = BitConverter.ToString(crypto);//把加密後的字串從Byte[]轉為字串
+            result = result.Replace("-", "").ToLower();
+            return result;
+        }
+
+        public string getMD5()
+        {
+            if (textBox1.Text == "")
+                return null;
+            string location = textBox1.Text;
+            byte[] apkByte = File.ReadAllBytes(location);
+            MD5 md5 = MD5.Create();//建立一個md5
+            byte[] crypto = md5.ComputeHash(apkByte);//進行md5加密
+            string result = BitConverter.ToString(crypto);//把加密後的字串從Byte[]轉為字串
+            result = result.Replace("-", "").ToLower();
+            return result;
+        }
+
+        private void txtFolderPath_DragDrop(object sender, DragEventArgs e)
+        {
+            String[] file = (String[])e.Data.GetData(DataFormats.FileDrop);
+            String dir = System.IO.Path.GetDirectoryName(file[0]);
+            String extension = System.IO.Path.GetExtension(file[0]).ToLower();
+            if (extension == ".ipa")
+            {
+                tbOutput.AppendText("ipa無法反組譯\r\n");
+                button4.Enabled = false;
+                this.tbOutputDir.Enabled = false;
+                this.tbOutputDir.Text = "";
+            }
+            else if (extension == ".apk")
+            {
+                button4.Enabled = true;
+                this.tbOutputDir.Enabled = true;
+            }
+            this.textBox1.Text = file[0];
+            this.tbOutputDir.Text = dir;
+            tbSHA1.Text = getSHA1();
+            tbMD5.Text = getMD5();
+        }
+
+        private void txtFolderPath_DragEnter(object sender, DragEventArgs e)
+        {
+            String[] file = (String[])e.Data.GetData(DataFormats.FileDrop);
+            String extension = System.IO.Path.GetExtension(file[0]);
+            extension = extension.ToLower();
+            if (extension == ".apk" || extension ==".ipa")
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            String file = openFileDialog1.FileName;
+            String extension = System.IO.Path.GetExtension(file);
+            String dir = System.IO.Path.GetDirectoryName(file);
+            if (result == DialogResult.OK)
+            {
+                if (extension == ".apk" || extension == ".APK")
+                {
+                    this.textBox1.Text = openFileDialog1.FileName;
+                    this.tbOutputDir.Text = dir;
+                    button4.Enabled = true;
+                    this.tbOutputDir.Enabled = true;
+                    tbSHA1.Text = getSHA1();
+                    tbMD5.Text = getMD5();
+                }
+                else if (extension == ".ipa")
+                {
+                    tbOutput.AppendText("ipa無法反組譯\r\n");
+                    button4.Enabled = false;
+                    this.textBox1.Text = openFileDialog1.FileName;
+                    this.tbOutputDir.Enabled = false;
+                    this.tbOutputDir.Text = "";
+                    tbSHA1.Text = getSHA1();
+                    tbMD5.Text = getMD5();
+                }
+                else 
+                {
+                    tbOutput.AppendText("不支援此檔案格式\r\n");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                tbOutputDir.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+
+        void process_Exited(object sender, EventArgs e)
+        {
+            MessageBox.Show("輸出完畢");
+        }
+        void proc_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            this.tbOutput.Invoke((MethodInvoker)delegate
+            {
+                tbOutput.AppendText(e.Data + "\r\n");
+            });
+        }
+        
+        void proc_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            process.Dispose();
+            process.Close();
+            this.tbOutput.Invoke((MethodInvoker)delegate
+            {
+                tbOutput.AppendText(e.Data + "\r\n");
+            });
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string args1 = textBox1.Text;
+            string args3 = textBox1.Text + ".jar";
+            string apkName = args1.Split('\\').Last<string>();
+            string args4 = tbOutputDir.Text + "\\" + apkName + ".exctracted\\source code";
+            string arg5 = tbOutputDir.Text + "\\" + apkName + ".exctracted\\manifest";
+
+
+            process = new Process();
+
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "tools\\decompiler.bat";
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            startInfo.Arguments = string.Format("\"{0}\" \"{1}\" \"{2}\" \"{3}\"", args1, args3, args4, arg5);
+
+            //tbOutput.Text = startInfo.Arguments;
+            startInfo.CreateNoWindow = true;
+            process.StartInfo = startInfo;
+            process.SynchronizingObject = this;
+
+
+            process.Start();
+            process.ErrorDataReceived += proc_ErrorDataReceived;
+            process.OutputDataReceived += proc_OutputDataReceived;
+            process.EnableRaisingEvents = true;
+            process.Exited += new EventHandler(process_Exited);
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || tbOutputDir.Text == "")
+            {
+                MessageBox.Show("請先選擇檔案與輸出資料夾");
+                return;
+            }
+            this.button1.Enabled = false;
+            this.button2.Enabled = false;
+            this.button4.Enabled = false;
+            this.progressBar1.Visible = true;
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.button1.Enabled = true;
+            this.button2.Enabled = true;
+            this.button4.Enabled = true;
+            this.progressBar1.Visible = false;
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.ToString());
+            }
+        }
+
+        
+    }
+}
