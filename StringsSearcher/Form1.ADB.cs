84974ef39d8486dbe0aa231add942154b17dfebd
ViperLiu <a0960330@gmail.com>
2018-08-23 11:42:15 +0800

    handle when adb can't find device

--- StringsSearcher/Form1.ADB.cs
+++ StringsSearcher/Form1.ADB.cs
@@ -17,7 +17,7 @@ namespace MASToolBox
         private void Btn_checkADB_Click(object sender, EventArgs e)
         {
             LibraryWorker adb = new LibraryWorker(Library.ADB);
-            adb.AddParam(new string[] { "shell", "\"getprop\"" });
+            adb.AddParam(new string[] { "-d", "shell", "getprop" });
             adb.SetOutputBox(rtb_adbOutput);
             adb.DataProcessor = AdbGetPhoneBrandModel;
             adb.JobFinished += PhoneFound;
@@ -26,23 +26,30 @@ namespace MASToolBox
 
         private void PhoneFound(object sender, EventArgs e)
         {
-            MessageBox.Show("Found Phone : " + PhoneBrand + " " + PhoneModel);
+            if (PhoneBrand == null)
+                MessageBox.Show("找不到可用的裝置", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+                MessageBox.Show("Found Phone : " + PhoneBrand + " " + PhoneModel);
         }
 
         private string AdbGetPhoneBrandModel(string data)
         {
+            if (data.StartsWith("error:"))
+            {
+                PhoneBrand = null;
+            }
             var tmp = data.Replace("[", "").Replace("]", "");
             if (tmp.StartsWith("ro.product.brand"))
             {
                 var brand = tmp.Split(':')[1].Trim();
                 PhoneBrand = brand.ToUpper();
-                return "Brand : " + brand;
+                return "Brand : " + PhoneBrand;
             }
             if(tmp.StartsWith("ro.product.model"))
             {
                 var model = tmp.Split(':')[1].Trim();
                 PhoneModel = model.ToUpper();
-                return "Model : " + model;
+                return "Model : " + PhoneModel;
             }
             return "";
         }
