5dc7b6971bf1513f19540a7d7259fa9050ed5c63
ViperLiu <a0960330@gmail.com>
2018-08-24 16:10:59 +0800

    取消"視窗最大化"功能

--- StringsSearcher/Form1.Designer.cs
+++ StringsSearcher/Form1.Designer.cs
@@ -1070,6 +1070,7 @@
             this.Controls.Add(this.tabControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MASToolBox";
